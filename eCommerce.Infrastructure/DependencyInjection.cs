c66419fa383c56246da031f5dae0a0d72ebf179f
Radhika <Radhi341@gmail.com>
2025-12-05 11:35:24 -0500

    Users Repository

--- eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +22,7 @@ namespace eCommerce.Infrastructure;
 
         // TO DO: Add services to the IoC container
         // Infrastructure services often include data access, caching and other low-level components.
+        services.AddSingleton<IUsersRepository, UsersRepository>();    
         return services;
     }
     }
---
+++ eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -0,0 +1,28 @@
+﻿
+using eCommerce.Core.DTO;
+using eCommerce.Core.Entities;
+using eCommerce.Core.RepositoryContracts;
+
+namespace eCommerce.Infrastructure.Repositories;
+
+internal class UsersRepository : IUsersRepository
+{
+    public async Task<ApplicationUser?> AddUser(ApplicationUser user)
+    {
+        // Generate a new unique UserID for the user
+        user.UserID = Guid.NewGuid();
+        return user;
+             
+    }
+
+    public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
+    {
+       return new ApplicationUser(){
+            UserID = Guid.NewGuid(),
+            Email = email,
+            Password = password,
+            PersonName = "",
+            Gender = GenderOptions.Male.ToString()
+        };
+    }
+}
